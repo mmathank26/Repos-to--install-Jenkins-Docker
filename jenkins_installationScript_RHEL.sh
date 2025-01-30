60ced0c06a01a72dbcffcd7afa9ddac467edade4
mmathank26 <167750099+mmathank26@users.noreply.github.com>
2025-01-30 14:41:12 +0530

    Add files via upload
    shell script to install Jenkins on RHEL vm.

---
+++ jenkins_installationScript_RHEL.sh
@@ -0,0 +1,54 @@
+sudo dnf update -y
+echo "#################################
+      #################################
+              System Update Done
+      #################################
+      #################################"
+
+echo "#################################
+      #################################
+           Adding Jenkins Repository
+      #################################
+      #################################"
+
+
+sudo dnf install wget -y
+
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+
+echo "#################################
+      #################################
+           Installing Java
+      #################################
+      #################################"
+
+
+sudo dnf install java -y
+
+
+echo "#################################
+      #################################
+           Adding Jenkins Repository
+      #################################
+      #################################"
+
+
+ sudo dnf install jenkins -y
+
+
+echo "#################################
+      #################################
+        Starting and enabling Jenkins
+      #################################
+      #################################"
+
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins 
+
+echo "########################################
+      ########################################
+      Jenkins Installation and Setup Completed
+      ########################################
+      ########################################"
