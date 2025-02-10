19bb857b8c731c3da7fb76efbc0403b6dcb20b7a
Meghal Mathankar <167750099+mmathank26@users.noreply.github.com>
2025-02-10 16:35:50 +0530

    Update docker_installationScript_RHEL.sh
    updated -y options for both the commands

--- docker_installationScript_RHEL.sh
+++ docker_installationScript_RHEL.sh
@@ -4,5 +4,5 @@ sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker
 sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
-sudo systemctl start docker
-sudo systemctl enable docker
+sudo systemctl start docker -y
+sudo systemctl enable docker -y
