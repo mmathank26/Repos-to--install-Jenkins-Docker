1292aa42eb9b01712db777f1de110aa35c280ec4
Meghal Mathankar <167750099+mmathank26@users.noreply.github.com>
2025-02-10 16:36:07 +0530

    Update docker_installationScript_RHEL.sh

--- docker_installationScript_RHEL.sh
+++ docker_installationScript_RHEL.sh
@@ -1,7 +1,7 @@
 sudo dnf -y install dnf-plugins-core
 sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
 
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 
 sudo systemctl start docker -y
